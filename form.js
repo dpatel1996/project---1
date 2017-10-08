86cae774f0bd7d1f6ba285bd8788f708c2ee9331
dpatel1996 <dpatel108@hawk.iit.edu>
2017-10-07 22:24:49 -0500

    trying to get JS into it

--- form.js
+++ form.js
@@ -1,1 +1,23 @@
 
+
+document.getElementById("form").onsubmit=function(){
+  errorForm()
+  }
+  function errorForm(){
+    alert("Application Successful");
+  }
+  
+  function c_fname(){
+    var fname = document.getElementById("f_name").value
+    if(fname == null)
+    {
+      alert("First name is missing");
+    }
+    var l_name = document.getElementById("l_name").value
+    if(lname == null)
+    {
+      alert(Last name is missing");
+    }
+  }
+  function submitForm()
+    alert("Thanks for applying. ") }
